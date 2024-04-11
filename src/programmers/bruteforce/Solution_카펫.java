b28598b77b2746c521f16d41d81b8f3f25582d2e
muuuumbi <dnpfkdlvm123@gmail.com>
2024-04-11 15:03:00 +0900

    카펫 풀이 완료

--- src/programmers/bruteforce/Solution_카펫.java
+++ src/programmers/bruteforce/Solution_카펫.java
@@ -1,4 +1,15 @@
 package programmers.bruteforce;
 
 public class Solution_카펫 {
+    public int[] solution(int brown, int yellow) {
+        int sumOfHoriVerti = brown / 2 - 2;
+        int[] answer = new int[2];
+        for(int verti = 1; verti <= sumOfHoriVerti - verti; verti++){
+            if(yellow == verti*(sumOfHoriVerti - verti)){
+                answer[0] = sumOfHoriVerti - verti + 2;
+                answer[1] = verti + 2;
+            }
+        }
+        return answer;
+    }
 }
